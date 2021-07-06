16379819e0994ff8900f34c5c902b25414ea853c
David Romaní <david@flux.cat>
2021-07-06 17:32:07 +0200

    improve menuLevel1Action controller

--- src/Controller/Frontend/DefaultController.php
+++ src/Controller/Frontend/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Frontend;
 
+use App\Entity\MenuLevel1;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,8 +20,9 @@ class DefaultController extends AbstractController
 
     /**
      * @Route("/{menu}", name="front_app_menu_level_1")
+     * @ParamConverter("menu", class="App\Entity\MenuLevel1", options={"mapping": {"menu": "slug"}})
      */
-    public function menuLevel1Action(string $menu): Response
+    public function menuLevel1Action(MenuLevel1 $menu): Response
     {
         return $this->render(
             'frontend/menu_level_1.html.twig',
