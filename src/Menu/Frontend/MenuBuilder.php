8aa1db10fba7a845fb025eb21d322a1bac8b260e
David Romaní <david@flux.cat>
2021-07-06 18:51:41 +0200

    improve menu builder - 1

--- src/Menu/Frontend/MenuBuilder.php
+++ src/Menu/Frontend/MenuBuilder.php
@@ -6,25 +6,41 @@ use App\Entity\MenuLevel1;
 use App\Repository\MenuLevel1Repository;
 use Knp\Menu\FactoryInterface;
 use Knp\Menu\ItemInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class MenuBuilder
 {
     private FactoryInterface $factory;
+    private RequestStack $requestStack;
 
-    public function __construct(FactoryInterface $factory)
+    public function __construct(FactoryInterface $factory, RequestStack $requestStack)
     {
         $this->factory = $factory;
+        $this->requestStack = $requestStack;
     }
 
     public function createMainMenu(MenuLevel1Repository $ml1r): ItemInterface
     {
+        $currentRoute = '';
+        $menuRoute = null;
+        if ($this->requestStack->getCurrentRequest()) {
+            $currentRoute = $this->requestStack->getCurrentRequest()->get('_route');
+            if ($this->requestStack->getCurrentRequest()->get('menu')) {
+                /** @var MenuLevel1 $menuRoute */
+                $menuRoute = $this->requestStack->getCurrentRequest()->get('menu');
+            }
+        }
         $menu = $this->factory->createItem('root');
         $menu->setChildrenAttribute('class', 'nav nav-pills');
-
-        $homepage = $menu->addChild('Home', ['route' => 'front_app_homepage']);
-        $homepage->setLinkAttribute('class', 'nav-link');
+        $homepage = $menu->addChild(
+            'home',
+            [
+                'label' => 'Home',
+                'route' => 'front_app_homepage',
+            ]
+        );
+        $homepage->setLinkAttribute('class', ($this->isHomepageRouteCurrent($currentRoute) ? 'nav-link active' : 'nav-link'));
         $homepage->setAttribute('class', 'nav-item');
-
         $ml1Items = $ml1r->getAllSortedByPosition()->getQuery()->getResult();
         /** @var MenuLevel1 $ml1Item */
         foreach ($ml1Items as $ml1Item) {
@@ -38,10 +54,20 @@ class MenuBuilder
                     ],
                 ]
             );
-            $item->setLinkAttribute('class', 'nav-link');
+            $item->setLinkAttribute('class', ($this->isMenuLevel1RouteCurrent($currentRoute) && $menuRoute && $menuRoute->getId() === $ml1Item->getId() ? 'nav-link active' : 'nav-link'));
             $item->setAttribute('class', 'nav-item');
         }
 
         return $menu;
+    }
+
+    private function isHomepageRouteCurrent(string $route): bool
+    {
+        return 'front_app_homepage' === $route;
+    }
+
+    private function isMenuLevel1RouteCurrent(string $route): bool
+    {
+        return 'front_app_menu_level_1' === $route || 'front_app_menu_level_2' === $route;
     }
 }
