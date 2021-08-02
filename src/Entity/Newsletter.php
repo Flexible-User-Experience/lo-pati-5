7874a05fbc3b47d88a826c9b9fad119faed1882f
David Romaní <david@flux.cat>
2021-08-02 13:28:26 +0200

    add Newsletter

---
+++ src/Entity/Newsletter.php
@@ -0,0 +1,186 @@
+<?php
+
+namespace App\Entity;
+
+use App\Enum\NewsletterStatusEnum;
+use App\Enum\NewsletterTypeEnum;
+use DateTimeInterface;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="App\Repository\NewsletterRepository")
+ */
+class Newsletter extends AbstractBase
+{
+    /**
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
+    private string $subject;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private ?DateTimeInterface $date = null;
+
+    /**
+     * @ORM\Column(type="integer", nullable=false, options={"default": 0})
+     */
+    private int $status = NewsletterStatusEnum::WAITING;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true, options={"default": 0})
+     */
+    private ?int $type = NewsletterTypeEnum::NEWS;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=false, options={"default": 0})
+     */
+    private bool $tested = false;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private ?DateTimeInterface $beginSend = null;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private ?DateTimeInterface $endSend = null;
+
+    /**
+     * var array|ArrayCollection.
+     *
+     * ORM\OneToMany(targetEntity="IsolatedNewsletterPost", mappedBy="newsletter", cascade={"persist", "remove"}, orphanRemoval=true)
+     * ORM\OrderBy({"position" = "ASC"})
+     */
+    private Collection $posts;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\NewsletterGroup")
+     * @ORM\JoinColumn(name="newsletter_group_id", referencedColumnName="id", nullable=true)
+     */
+    private ?NewsletterGroup $group = null;
+
+    public function __construct()
+    {
+        $this->posts = new ArrayCollection();
+    }
+
+    public function getSubject(): string
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(string $subject): self
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    public function getDate(): ?DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(?DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getStatus(): int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getType(): ?int
+    {
+        return $this->type;
+    }
+
+    public function setType(?int $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function isTested(): bool
+    {
+        return $this->tested;
+    }
+
+    public function setTested(bool $tested): self
+    {
+        $this->tested = $tested;
+
+        return $this;
+    }
+
+    public function getBeginSend(): ?DateTimeInterface
+    {
+        return $this->beginSend;
+    }
+
+    public function setBeginSend(?DateTimeInterface $beginSend): self
+    {
+        $this->beginSend = $beginSend;
+
+        return $this;
+    }
+
+    public function getEndSend(): ?DateTimeInterface
+    {
+        return $this->endSend;
+    }
+
+    public function setEndSend(?DateTimeInterface $endSend): self
+    {
+        $this->endSend = $endSend;
+
+        return $this;
+    }
+
+    public function getPosts(): ?Collection
+    {
+        return $this->posts;
+    }
+
+    public function setPosts(?Collection $posts): self
+    {
+        $this->posts = $posts;
+
+        return $this;
+    }
+
+    // TODO add/remove Post
+
+    public function getGroup(): ?NewsletterGroup
+    {
+        return $this->group;
+    }
+
+    public function setGroup(?NewsletterGroup $group): self
+    {
+        $this->group = $group;
+
+        return $this;
+    }
+
+    public function __toString(): string
+    {
+        return $this->date ? $this->getDate()->format('d/m/Y').' · '.$this->subject : ($this->id ? $this->subject : '---');
+    }
+}
---
+++ src/Repository/NewsletterRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Newsletter;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Newsletter|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Newsletter|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Newsletter[]    findAll()
+ * @method Newsletter[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class NewsletterRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Newsletter::class);
+    }
+}
