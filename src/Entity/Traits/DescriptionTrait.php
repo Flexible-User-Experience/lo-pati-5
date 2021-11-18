79c7f214bb18568c6fad03302d64ee57b888daa6
David Romaní <david@flux.cat>
2021-11-18 19:33:36 +0100

    add PageTranslation class

--- src/Entity/Traits/DescriptionTrait.php
+++ src/Entity/Traits/DescriptionTrait.php
@@ -3,11 +3,13 @@
 namespace App\Entity\Traits;
 
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 trait DescriptionTrait
 {
     /**
      * @ORM\Column(type="text", length=4000)
+     * @Gedmo\Translatable
      */
     private string $description;
 
