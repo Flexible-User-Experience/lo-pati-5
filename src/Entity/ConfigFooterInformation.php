444921dcd2c7d671562d8494223c31a334177e36
David Romaní <david@flux.cat>
2021-11-19 10:30:11 +0100

    add ConfigFooterInformationTranslation class

--- src/Entity/ConfigFooterInformation.php
+++ src/Entity/ConfigFooterInformation.php
@@ -2,37 +2,56 @@
 
 namespace App\Entity;
 
+use App\Entity\Traits\TranslationsTrait;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Table()
  * @ORM\Entity(repositoryClass="App\Repository\ConfigFooterInformationRepository")
- * Gedmo\TranslationEntity(class="LoPati\BlogBundle\Entity\Translation\ConfiguracioTranslation")
+ * @Gedmo\TranslationEntity(class="App\Entity\Translation\ConfigFooterInformationTranslation")
  */
 class ConfigFooterInformation extends AbstractBase
 {
+    use TranslationsTrait;
+
     /**
      * @ORM\Column(type="text", length=10000, nullable=true)
+     * @Gedmo\Translatable
      */
     private ?string $address = null;
 
     /**
      * @ORM\Column(type="text", length=10000, nullable=true)
-     * Gedmo\Translatable
+     * @Gedmo\Translatable
      */
     private ?string $timetable = null;
 
     /**
      * @ORM\Column(type="text", length=10000, nullable=true)
-     * Gedmo\Translatable
+     * @Gedmo\Translatable
      */
     private ?string $organizer = null;
 
     /**
      * @ORM\Column(type="text", length=10000, nullable=true)
-     * Gedmo\Translatable
+     * @Gedmo\Translatable
      */
     private ?string $collaborator = null;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Translation\ConfigFooterInformationTranslation", mappedBy="object", cascade={"persist", "remove"})
+     * @Assert\Valid()
+     */
+    private ?Collection $translations;
+
+    public function __construct()
+    {
+        $this->translations = new ArrayCollection();
+    }
 
     public function getAddress(): ?string
     {
