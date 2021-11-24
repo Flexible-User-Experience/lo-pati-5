386cfb82ce62713df0f8cb6563ebd8e6e4c99fc5
David Romaní <david@flux.cat>
2021-11-24 18:01:53 +0100

    improve frontend artist detail view

--- src/Controller/Frontend/ArtistController.php
+++ src/Controller/Frontend/ArtistController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller\Frontend;
 
-use App\Entity\Archive;
 use App\Entity\Artist;
 use App\Entity\MenuLevel1;
+use App\Entity\MenuLevel2;
+use App\Repository\ArtistRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,16 +19,29 @@ final class ArtistController extends AbstractController
      * @Route({"ca": "/{menu}/{submenu}/artista/{slug}", "es": "/{menu}/{submenu}/{slug}"}, name="front_app_artist_detail")
      * @Entity("submenu", class="App\Entity\MenuLevel2", expr="repository.getByMenuAndSubmenuSlugs(menu, submenu)")
      * @ParamConverter("menu", class="App\Entity\MenuLevel1", options={"mapping": {"menu": "slug"}})
-     * @ParamConverter("artist", class="App\Entity\Archive", options={"mapping": {"slug": "slug"}})
      */
-    public function artistDetail(MenuLevel1 $menu, Archive $archive, Artist $artist, KernelInterface $kernel): Response
+    public function artistDetail(MenuLevel1 $menu, MenuLevel2 $submenu, string $slug, ArtistRepository $ar, KernelInterface $kernel): Response
     {
+        $artists = $ar->getEnabledSortedByName()->getQuery()->getResult();
+        $found = false;
+        /** @var Artist $artist */
+        foreach ($artists as $artist) {
+            if ($artist->getSlug() === $slug) {
+                $found = true;
+                break;
+            }
+        }
+        if (!$found) {
+            $this->createNotFoundException();
+        }
+
         return $this->render(
             'frontend/artist/artist_detail.html.twig',
             [
                 'menu' => $menu,
-                'archive' => $archive,
-                'page' => $page,
+                'submenu' => $submenu,
+                'artists' => $artists,
+                'artist' => $artist,
                 'show_debug_page_info' => $kernel->isDebug(),
             ]
         );
