d142f770bda2aee0ffed6156b4785e5263b17ab7
David Romaní <david@flux.cat>
2022-01-12 18:48:39 +0100

    replace to reserve by contact us link in frontend footer

--- src/Controller/Frontend/VisitingHoursController.php
+++ src/Controller/Frontend/VisitingHoursController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller\Frontend;
 
+use App\Repository\PageRepository;
 use App\Repository\VisitingHoursRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,12 +14,13 @@ final class VisitingHoursController extends AbstractController
     /**
      * @Route("/visiting-hours/timetable", name="front_app_visiting_hours_timetable", priority=10)
      */
-    public function timetable(VisitingHoursRepository $vhr): Response
+    public function timetable(VisitingHoursRepository $vhr, PageRepository $pr, ParameterBagInterface $pb): Response
     {
         return $this->render(
             'frontend/visiting_hours/timetable.html.twig',
             [
                 'visiting_hours' => $vhr->getVisitingHours(),
+                'contact_page' => $pr->find($pb->get('id_page_contact')),
             ]
         );
     }
