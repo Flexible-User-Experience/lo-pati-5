dfce0148bef47bc76351e9cc28f7c531a576fd7b
David Romaní <david@flux.cat>
2022-01-13 10:11:38 +0100

    add menu level 1 color in admin edit view

--- src/Admin/MenuLevel1Admin.php
+++ src/Admin/MenuLevel1Admin.php
@@ -12,6 +12,7 @@ use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ColorType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
@@ -145,6 +146,13 @@ final class MenuLevel1Admin extends AbstractBaseAdmin
             )
             ->end()
             ->with('admin.common.controls', $this->getFormMdSuccessBoxArray(3))
+            ->add(
+                'color',
+                ColorType::class,
+                [
+                    'required' => true,
+                ]
+            )
             ->add(
                 'position',
                 NumberType::class,
